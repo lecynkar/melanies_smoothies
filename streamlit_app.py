3c5049ba50968b0ff78486e83de03185bcc67714
lecynkar <46504897+lecynkar@users.noreply.github.com>
2025-01-28 12:24:01 -0500

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be:', name_on_order)
